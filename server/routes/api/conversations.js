4b623a6fd43af22ec3b0986707c4e2a8e6d7df5b
Muhammad Almas Khan <46604137+almaskhanwazir@users.noreply.github.com>
2021-08-30 16:33:56 +0500

    messages arrangement and  go to bottom on click conversation

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -5,6 +5,7 @@ const onlineUsers = require("../../onlineUsers");
 
 // get all conversations for a user, include latest message text for preview, and all messages
 // include other user model so we have info on username/profile pic (don't include current user info)
+// TODO: for scalability, implement lazy loading
 router.get("/", async (req, res, next) => {
   try {
     if (!req.user) {
@@ -19,9 +20,8 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message},
         {
           model: User,
           as: "user1",
@@ -45,6 +45,10 @@ router.get("/", async (req, res, next) => {
           required: false,
         },
       ],
+      order: [
+        ['createdAt', 'DESC'],
+        [ {model: Message}, 'createdAt', 'ASC']
+    ]
     });
 
     for (let i = 0; i < conversations.length; i++) {
@@ -68,7 +72,7 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length-1].text;
       conversations[i] = convoJSON;
     }
 
